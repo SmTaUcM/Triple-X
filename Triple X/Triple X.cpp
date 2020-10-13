5d32a31b3ec251dbcaec96ed342759a13d5f44c0
Stu Macintosh <SmTaUcM@Yahoo.co.uk>
2020-10-13 14:29:31 +0100

    Section 2 finished.

--- Triple X/Triple X.cpp
+++ Triple X/Triple X.cpp
@@ -1,27 +1,27 @@
 // Preprocessor Directives (Imports):
 // iostream is a header file.
 #include <iostream>
+#include <ctime>
 
-
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
     // Print welcome messages to the terminal.
     // cout = 'Character Output' and << is known as the 'Insertion Operator'.
-    std::cout << "You are a secret agent breaking into a secure server room...\n";
-    std::cout << "You need to enter the correct codes to continue...\n\n";
+    std::cout << "\n\nYou are a secret agent breaking into a level " << Difficulty;
+    std::cout << " secure server room...\nYou need to enter the correct codes to continue...\n\n";
 }
 
 
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
 
     // Declare and initialise variables.
     // Declare three numhber code.
-    const int CodeA = 4;
-    const int CodeB = 3;
-    const int CodeC = 2;
+    const int CodeA = rand() % Difficulty + Difficulty; // rand() returns a value from 0 to 32,767
+    const int CodeB = rand() % Difficulty + Difficulty; // + 1 stops any number being 0.
+    const int CodeC = rand() % Difficulty + Difficulty; // % 1 only returns 0, % 2 only returns 0, 1 etc...
 
     const int CodeSum = CodeA + CodeB + CodeC;
     const int CodeProduct = CodeA * CodeB * CodeC;
@@ -41,15 +41,17 @@ void PlayGame()
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
-
+    
     // Check to see if the player's input is correct.
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "\nYou win!";
+        std::cout << "\n*** Well done agent! You have extracted a file! Keep going! ***";
+        return true;
     }
     else
     {
-        std::cout << "\nYou lose!";
+        std::cout << "\n*** You entered the wrong code! Careful agent! Try again! ***";
+        return false;
     }
 }
 
@@ -58,7 +60,27 @@ void PlayGame()
 int main()
 // Main Program - main() is the entry point for every C++ program and must have one to compile.:
 {
-    PlayGame();
+    srand(time(NULL)); // Produce a random seed based on the time of the day.
+
+    // Declare and initialise variables.
+    int LevelDifficulty = 1;
+    const int MaxDifficulty = 5;
+
+    while (LevelDifficulty <= MaxDifficulty) // Loop the game until all levels are completed.
+    {
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        // .clear() and .ignore() reset 'cin' so that it is not awaiting input if a char is entered and not an int.
+        std::cin.clear(); // Clears any cin errors.
+        std::cin.ignore(); // Discards the buffer / stream.
+
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+    }
+    
+    // Display the game winning message.
+    std::cout << "\n*** Great work agent! You got all the files! Now get out of there! ***\n";
     // Return - This is mandantory.
     return 0;
 }
