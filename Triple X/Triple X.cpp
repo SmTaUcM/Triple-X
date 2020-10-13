b86cd3d6de34dd598bb223ff5f2f83783e55ba8a
Stu Macintosh <SmTaUcM@Yahoo.co.uk>
2020-10-13 09:47:57 +0100

    Section 19.

--- Triple X/Triple X.cpp
+++ Triple X/Triple X.cpp
@@ -5,6 +5,22 @@
 // Main Program:
 int main()
 {
-    std::cout << "Hello, World!\n";
+    std::cout << "You are a secret agent breaking into a secure server room...";
+    std::cout << std::endl;
+    std::cout << "You need to enter the correct codes to continue...";
+
+    // Declare and initialise variables.
+    const int a = 4;
+    const int b = 3;
+    const int c = 2;
+
+    const int sum = a + b + c;
+    const int product = a * b * c;
+
+    std::cout << std::endl;
+    std::cout << sum << std::endl;
+    std::cout << product << std::endl;
+
+    // Return.
     return 0;
 }
